0f51003ece6991a2c0cdce75bc63867c397d3f8e
David Hewitt <hello@wp-unite.io>
2025-05-22 09:32:33 +0100

    Add lead

--- src/main.tsx
+++ src/main.tsx
@@ -1,59 +1,68 @@
-import { connect, Field, FieldIntentCtx } from 'datocms-plugin-sdk';
-import ConfigScreen from './entrypoints/ConfigScreen';
-import { render } from './utils/render';
-import 'datocms-react-ui/styles.css';
+import { connect, type Field, type FieldIntentCtx } from "datocms-plugin-sdk";
+import ConfigScreen from "./entrypoints/ConfigScreen";
+import { render } from "./utils/render";
+import "datocms-react-ui/styles.css";
 
 connect({
-  renderConfigScreen(ctx) {
-    return render(<ConfigScreen ctx={ctx} />);
-  },
-  customBlockStylesForStructuredTextField(_field: Field, _ctx: FieldIntentCtx) {
-    return [
-      {
-        id: 'blockquote',
-        label: 'Blockquote',
-        node: 'paragraph',
-        appliedStyle: {
-          borderLeft: '5px solid #000',
-          paddingLeft: '20px',
-          display: 'block',
-          fontStyle: 'italic',
-        },
-      },
-    ];
-  },
-  customMarksForStructuredTextField(_field: Field, _ctx: FieldIntentCtx) {
-    return [
-      {
-        id: 'small',
-        label: 'Small',
-        icon: 'text-size',
-        appliedStyle: {
-          fontSize: '0.85rem',
-        },
-      },
-      {
-        id: 'subscript',
-        label: 'Subscript',
-        icon: 'subscript',
-        keyboardShortcut: 'ctrl+shift+d',
-        appliedStyle: {
-          verticalAlign: 'sub',
-          fontSize: '0.5rem',
-        },
-      },
-      {
-        id: 'superscript',
-        label: 'Superscript',
-        icon: 'superscript',
-        keyboardShortcut: 'ctrl+shift+u',
-        appliedStyle: {
-          verticalAlign: 'top',
-          fontSize: '0.5rem',
-          position: 'relative',
-          top: '-1px',
-        },
-      },
-    ];
-  },
+	renderConfigScreen(ctx) {
+		return render(<ConfigScreen ctx={ctx} />);
+	},
+	customBlockStylesForStructuredTextField(_field: Field, _ctx: FieldIntentCtx) {
+		return [
+			{
+				id: "lead",
+				label: "Lead",
+				node: "paragraph",
+				appliedStyle: {
+					fontSize: "18px",
+					lineHeight: "20px",
+				},
+			},
+			{
+				id: "blockquote",
+				label: "Blockquote",
+				node: "paragraph",
+				appliedStyle: {
+					borderLeft: "5px solid #000",
+					paddingLeft: "20px",
+					display: "block",
+					fontStyle: "italic",
+				},
+			},
+		];
+	},
+	customMarksForStructuredTextField(_field: Field, _ctx: FieldIntentCtx) {
+		return [
+			{
+				id: "small",
+				label: "Small",
+				icon: "text-size",
+				appliedStyle: {
+					fontSize: "0.85rem",
+				},
+			},
+			{
+				id: "subscript",
+				label: "Subscript",
+				icon: "subscript",
+				keyboardShortcut: "ctrl+shift+d",
+				appliedStyle: {
+					verticalAlign: "sub",
+					fontSize: "0.5rem",
+				},
+			},
+			{
+				id: "superscript",
+				label: "Superscript",
+				icon: "superscript",
+				keyboardShortcut: "ctrl+shift+u",
+				appliedStyle: {
+					verticalAlign: "top",
+					fontSize: "0.5rem",
+					position: "relative",
+					top: "-1px",
+				},
+			},
+		];
+	},
 });
